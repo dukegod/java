d28abcdf3e71c3bd78bbc31098acaf8c2841fee9
dukegod <dukehui1991@gmail.com>
2018-04-25 17:45:53 +0800

    java kit

---
+++ src/com/javaextends/Baseclass.java
@@ -0,0 +1,12 @@
+package com.javaextends;
+
+public class Baseclass {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String n) {
+        this.name = n;
+    }
+}
---
+++ src/com/javaextends/Secondclass.java
@@ -0,0 +1,13 @@
+package com.javaextends;
+
+public class Secondclass extends Baseclass {
+    String company;
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+}
