ee0d1bf92bb2aa9ee1d426cb2d28e5f289b82922
dukegod <dukehui1991@gmail.com>
2019-06-26 20:59:35 +0800

    java basic

--- src/com/javaextends/Main.java
+++ src/com/javaextends/Main.java
@@ -1,7 +1,18 @@
 package com.javaextends;
 
 public class Main {
+
     public static void main(String[] args) {
+        int age;
+        age = 18;
+        Boolean bl;
+        bl = false;
+        String str;
+        str = "hello";
+
+        System.out.println(str);
+        System.out.println(bl);
+        System.out.println(age);
         System.out.println("Hello World!");
     }
 }
